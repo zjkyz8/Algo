e3991c0d046318359c7cc00794ed565a3e1c2a4f
Xin Huang <xinhuang@thoughtworks.com>
2013-03-09 14:08:06 +0800

    [xhuang]: add insertion sorting project.

---
+++ src/InsertingSorting/InsertSorting.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace InsertingSorting
+{
+    public class InsertSorting
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        public static int[] 
+        {
+        }
+    }
+}
