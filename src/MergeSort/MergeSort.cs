e8b19b771fefbe979c8718f19e947cb06f999332
PHOENIX <zjkyz8@gmail.com>
2013-04-14 22:00:41 +0800

    MergeSort

---
+++ src/MergeSort/MergeSort.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace MergeSort
+{
+    public class MergeSort
+    {
+        public static int[] Sort(int[] seqInt, long head, long tail)
+        {
+            long arrayLength = tail - head + 1;
+            var result = new int[arrayLength];
+            if (tail - head <= 0)
+            {
+                Array.Copy(seqInt, head, result, 0, tail - head + 1);
+                return result;
+            }
+            int[] firstPart = Sort(seqInt, head, (head + tail)/2);
+            int[] secondPart = Sort(seqInt, (head + tail)/2 + 1, tail);
+
+            long firstIndex = 0, secondIndex = 0;
+            for (long i = 0; i < arrayLength; i++)
+            {
+                if (secondIndex >= secondPart.Length)
+                {
+                    result[i] = firstPart[firstIndex];
+                    firstIndex++;
+                }
+                else if (firstIndex >= firstPart.Length)
+                {
+                    result[i] = secondPart[secondIndex];
+                    secondIndex++;
+                }
+                else if (firstPart[firstIndex] < secondPart[secondIndex])
+                {
+                    result[i] = firstPart[firstIndex];
+                    firstIndex++;
+                }
+                else
+                {
+                    result[i] = secondPart[secondIndex];
+                    secondIndex++;
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ test/MergeSortTest/MergeSortTest.cs
@@ -0,0 +1,50 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace MergeSortTest
+{
+    public class MergeSortTest
+    {
+        [Test]
+        public void should_reuturn_iteself_when_array_contain_one_item()
+        {
+            var intSeq = new[] {2};
+
+            MergeSort.MergeSort.Sort(intSeq, 0, intSeq.Length-1).Should().Equal(intSeq);
+        }
+
+        [Test]
+        public void should_return_array_with_asc_order_when_contain_two_items_with_des_order()
+        {
+            var intSeq = new[] {2, 1};
+
+            int[] result = MergeSort.MergeSort.Sort(intSeq, 0, intSeq.Length-1);
+
+            result.Should().Contain(intSeq);
+            result.Should().BeInAscendingOrder();
+        }
+
+        [Test]
+        public void should_return_array_with_asc_order_when_contain_5_items_with_asc_order()
+        {
+            var intSeq = new[] { 2, 5, 10, 11, 19 };
+
+            int[] result = MergeSort.MergeSort.Sort(intSeq, 0, intSeq.Length - 1);
+
+            result.Should().Contain(intSeq);
+            result.Should().BeInAscendingOrder();
+        }
+
+        [Test]
+        public void should_return_array_with_asc_order_when_contain_11_items_without_order()
+        {
+            var intSeq = new[] { 11, 5, 10, 2, 7, 19, 18, 3, 1, 8, 4 };
+
+            int[] result = MergeSort.MergeSort.Sort(intSeq, 0, intSeq.Length - 1);
+
+            result.Should().Contain(intSeq);
+            result.Should().BeInAscendingOrder();
+        }
+
+    }
+}
