f95bd5af6ded5133ca29a62ef076993182fbec77
PHOENIX <zjkyz8@gmail.com>
2013-07-23 20:26:41 +0800

    bubble sort

---
+++ BubbleSort/BubbleSort.cs
@@ -0,0 +1,22 @@
+﻿namespace BubbleSort
+{
+    public class BubbleSort
+    {
+        public int[] Sort(int[] array)
+        {
+            for (var outter = 1; outter < array.Length; outter++ )
+            {
+                for (int innter = array.Length-1; innter >= outter; innter--)
+                {
+                    if (array[innter] < array[innter - 1])
+                    {
+                        var temp = array[innter - 1];
+                        array[innter - 1] = array[innter];
+                        array[innter] = temp;
+                    }
+                }
+            }
+            return array;
+        }
+    }
+}
---
+++ BubbleSortTest/BubbleSortTest.cs
@@ -0,0 +1,44 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace BubbleSortTest
+{
+    public class BubbleSortTest
+    {
+        [Test]
+        public void should_return_array_with_correct_order_when_given_array_contain_two_ordered_items()
+        {
+            var array = new[] {1, 2};
+
+            var orderedArray = new BubbleSort.BubbleSort().Sort(array);
+            orderedArray.Should().BeInAscendingOrder();
+        }
+
+        [Test]
+        public void should_return_array_with_correct_order_when_given_array_contain_two_non_ordered_items()
+        {
+            var array = new[] { 2, 1 };
+
+            var orderedArray = new BubbleSort.BubbleSort().Sort(array);
+            orderedArray.Should().BeInAscendingOrder();
+        }
+
+        [Test]
+        public void should_return_array_with_correct_order_when_given_array_contain_three_non_ordered_items()
+        {
+            var array = new[] {3, 2, 1 };
+
+            var orderedArray = new BubbleSort.BubbleSort().Sort(array);
+            orderedArray.Should().BeInAscendingOrder();
+        }
+
+        [Test]
+        public void should_return_array_with_correct_order_when_given_array_contain_five_non_ordered_items()
+        {
+            var array = new[] { 3, 2, 1, 5, 4 };
+
+            var orderedArray = new BubbleSort.BubbleSort().Sort(array);
+            orderedArray.Should().BeInAscendingOrder();
+        }
+    }
+}
