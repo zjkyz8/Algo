5258707da7d221b8d22de912e78d269c161011c1
PHOENIX <zjkyz8@gmail.com>
2013-07-24 21:25:44 +0800

    binary search

---
+++ src/BinarySearch/BinarySearch.cs
@@ -0,0 +1,22 @@
+﻿namespace BinarySearch
+{
+    public class BinarySearch
+    {
+        public int Search(int[] array, int value)
+        {
+            return Search(array, value, 0, array.Length - 1);
+        }
+
+        private int Search(int[] array, int value, int headIndex, int tailIndex)
+        {
+            if (headIndex > tailIndex)
+                return -1;
+
+            var middleIndex = (headIndex + tailIndex)/2;
+            var middleValue = array[middleIndex];    
+            if (value < middleValue)
+                return Search(array, value, headIndex, middleIndex);
+            return value>middleValue ? Search(array, value, middleIndex, tailIndex) : middleIndex;
+        }
+    }
+}
---
+++ test/BinarySearch/BinarySearshTest.cs
@@ -0,0 +1,22 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace BinarySearchTest
+{
+    public class BinarySearshTest
+    {
+        [Test]
+        public void should_return_0_first_one_is_the_value_()
+        {
+            var array = new[] {1, 6, 9, 73, 90};
+            new BinarySearch.BinarySearch().Search(array, 1).Should().Be(0);
+        }
+
+        [Test]
+        public void should_return_3_third_one_is_the_value_()
+        {
+            var array = new[] { 1, 6, 9, 73, 90 };
+            new BinarySearch.BinarySearch().Search(array, 73).Should().Be(3);
+        }
+    }
+}
