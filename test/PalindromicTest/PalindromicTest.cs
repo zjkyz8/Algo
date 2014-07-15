97b9d147dd99470f5586a3cff844aa93e9b1a2b3
Huang, Xin <xinhuang@thoughtworks.com>
2014-07-16 07:39:06 +0800

    [xin] reorganize code structure

--- test/PalindromicTest/PalindromicTest.cs
+++ test/PalindromicTest/PalindromicTest.cs
@@ -1,4 +1,6 @@
-﻿using NUnit.Framework;
+﻿using System;
+using FluentAssertions;
+using NUnit.Framework;
 
 namespace PalindromicTest
 {
@@ -7,30 +9,31 @@ namespace PalindromicTest
         [Test]
         public void should_return_true_when_one_digit()
         {
-//            Palindromic.Palindromic.Test(7).Should().BeTrue();
-            Assert.IsTrue(Palindromic.Palindromic.Test(7));
+            Palindromic.Palindromic.Test(new Random().Next(0, 9)).Should().BeTrue();
         }
 
         [Test]
-        public void should_return_false_when_two_different_digit()
+        public void should_return_true_when_three_digit_palindromic()
         {
-            //            Palindromic.Palindromic.Test(7).Should().BeTrue();
-            Assert.IsFalse(Palindromic.Palindromic.Test(72));
+            Palindromic.Palindromic.Test(787).Should().BeTrue();
         }
 
         [Test]
-        public void should_return_false_when_three_different_digit()
+        public void should_return_false_when_three_digit_non_palindromic()
         {
-            //            Palindromic.Palindromic.Test(7).Should().BeTrue();
-            Assert.IsFalse(Palindromic.Palindromic.Test(728));
+            Palindromic.Palindromic.Test(733).Should().BeFalse();
         }
 
         [Test]
-        public void should_return_false_when_three_same_digit()
+        public void should_return_false_when_two_digit_non_palindromic()
         {
-            //            Palindromic.Palindromic.Test(7).Should().BeTrue();
-            Assert.IsTrue(Palindromic.Palindromic.Test(727));
+            Palindromic.Palindromic.Test(72).Should().BeFalse();
         }
 
+        [Test]
+        public void should_return_true_when_two_digit_palindromic()
+        {
+            Palindromic.Palindromic.Test(77).Should().BeTrue();
+        }
     }
 }
